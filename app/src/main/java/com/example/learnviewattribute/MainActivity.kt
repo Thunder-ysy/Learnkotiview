ce64f96914787716a36ae9d5aaaee7c96123473c
Balaji i5 7thg en <allindiaeditor123@gmail.com>
2023-07-06 09:44:48 +0530

    2nd Add Activity

--- app/src/main/java/com/example/learnviewattribute/MainActivity.kt
+++ app/src/main/java/com/example/learnviewattribute/MainActivity.kt
@@ -1,18 +1,51 @@
 package com.example.learnviewattribute
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        // Declare Views
         val textvidew:TextView= findViewById(R.id.textView)
-        textvidew.setText("Rate Us 5 Star for my Video")
+        val edittxt:EditText=findViewById(R.id.editText)
+        val mybutton=findViewById<Button>(R.id.clbutton)
+        val bugonext=findViewById<Button>(R.id.gonext)
+
+
+
+
+
+
+
+
+        // *Adding Functionlity
+
+//        ** Set Text From Edit Box to TExt Box By Pressing A Button
+
+        mybutton.setOnClickListener(){
+            val mytext=edittxt.text
+            textvidew.setText(mytext)
+
+//          textvidew.setText
+            Toast.makeText(this@MainActivity,"The Button is Clicked",Toast.LENGTH_LONG).show()
+        }
+
+//        ** Go To Next Activity by Clicking on A Button
+        bugonext.setOnClickListener(){
+            val intent=Intent(this,SecondScreen::class.java)
+            startActivity(intent)
+        }
+
 
     }
 }
---
+++ app/src/main/java/com/example/learnviewattribute/SecondScreen.kt
@@ -0,0 +1,15 @@
+package com.example.learnviewattribute
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SecondScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_screen)
+
+        // step 2
+        val mybutton=findViewById<Button>(R.id.)
+    }
+}
