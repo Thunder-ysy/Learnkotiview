55a10d4a83327841f1c3a79037c23f04b9bd2e36
Balaji i5 7thg en <allindiaeditor123@gmail.com>
2023-07-07 07:53:06 +0530

    3rd Counter App

--- app/src/main/java/com/example/learnviewattribute/SecondScreen.kt
+++ app/src/main/java/com/example/learnviewattribute/SecondScreen.kt
@@ -3,13 +3,26 @@ package com.example.learnviewattribute
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class SecondScreen : AppCompatActivity() {
+    var counter:Int=0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second_screen)
 
         // step 2
-        val mybutton=findViewById<Button>(R.id.)
+        val mybutton=findViewById<Button>(R.id.button)
+        val results:TextView=findViewById(R.id.textView2)
+
+
+        // Step 3
+       mybutton.setOnClickListener(){
+           results.setText(" "+increaseCounter())
+       }
+    }
+    fun increaseCounter():Int {
+        counter++
+        return counter
     }
 }
